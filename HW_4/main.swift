da6b5206da2fa0931655e10a4e5bf40380e2cb3d
Josh Malters <frostymalts@gmail.com>
2015-10-13 19:38:59 -0700

    Initial Commit

---
+++ HW_4/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HW_4
+//
+//  Created by Joshua Malters on 10/13/15.
+//  Copyright © 2015 Joshua Malters. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
